c12b34b296a5de04411b9846935da96adbac634f
Nesmy Point du Jour <pnesmy@gmail.com>
2024-01-27 08:06:54 -0500

    Init raylib

---
+++ build/CMakeFiles/PingPong.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PingPong.dir/src/Game.cpp.o"
+  "CMakeFiles/PingPong.dir/src/Game.cpp.o.d"
+  "PingPong"
+  "PingPong.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PingPong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Game.cpp
@@ -0,0 +1,11 @@
+#include "raylib.h"
+
+int main(int argc, char *argv[]) {
+  InitWindow(640, 455, "PingPong");
+
+  while (!WindowShouldClose()) {
+  }
+
+  CloseWindow();
+  return 0;
+}
